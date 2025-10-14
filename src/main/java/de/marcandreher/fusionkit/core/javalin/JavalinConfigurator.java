d2010dc694063768073b62775884dfe2d94ad788
marcandreher <marcandreher@users.noreply.github.com>
2025-10-14 11:28:09 +0000

    deploy: 28438982569e374869522df771eaf460d4073db2

--- src/main/java/de/marcandreher/fusionkit/core/javalin/JavalinConfigurator.java
+++
@@ -1,7 +0,0 @@
-package de.marcandreher.fusionkit.core.javalin;
-
-import io.javalin.config.JavalinConfig;
-
-public interface JavalinConfigurator {
-    public void configure(JavalinConfig config);
-}
